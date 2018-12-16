a02824f354982ad40900279d5d03640f11fefc5c
taesch124 <taesch124@gmail.com>
2018-12-16 17:55:31 -0500

    Adding JAWDB Config

--- burger/config/connection.js
+++ burger/config/connection.js
@@ -1,11 +1,17 @@
 const mysql = require('mysql');
+var connection;
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'root',
-    database: 'burgers_db'
-});
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'root',
+        database: 'burgers_db'
+    });
+}
+
 
 module.exports = connection;
